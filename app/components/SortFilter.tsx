c7c273d5cdf9df61b60a158dd99c61aa858e5023
Vadim <--global>
2024-09-06 10:00:10 -0700

    update routes

--- app/components/SortFilter.tsx
+++ app/components/SortFilter.tsx
@@ -20,7 +20,7 @@ import type {
 import {
   AppliedFilter,
   SortParam,
-} from '~/routes/$locale.collections.$collectionHandle';
+} from '~/routes/($locale).collections.$collectionHandle';
 
 type Props = {
   filters: Filter[];
