5d6e1d9764d863b00fcc8570b8e03facf4feb1cd
Vadim <--global>
2024-11-04 18:25:55 -0800

    update: top offset in the desktop menu

--- app/components/DropdownMenu.tsx
+++ app/components/DropdownMenu.tsx
@@ -1,4 +1,4 @@
-import {Fragment, ReactNode} from 'react';
+import {Fragment, ReactNode, useEffect, useState} from 'react';
 import {Menu, Transition} from '@headlessui/react';
 
 type Props = {
@@ -7,6 +7,22 @@ type Props = {
 };
 
 export function DropdownMenu({menuTitle, children}: Props) {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+      const handleResize = () => {
+          setWindowWidth(window.innerWidth);
+      };
+
+      // Add event listener
+      window.addEventListener('resize', handleResize);
+
+      // Cleanup event listener on unmount
+      return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  const topOffset = windowWidth < 1280 ? '90px' : '140px';
+
   return (
     <Menu as={Fragment}>
       <Menu.Button className="uppercase">{menuTitle}</Menu.Button>
@@ -23,7 +39,7 @@ export function DropdownMenu({menuTitle, children}: Props) {
           className={`fixed right-0 md:top-narrow-sticky xxl:top-wide-sticky w-screen h-screen-no-nav-mobile xl:h-screen-no-nav-desktop bg-white pb-10 max-w-menu-width ${
             menuTitle == 'SALES' ? 'hidden' : ''
           }`}
-          style={{top:'90px'}}
+          style={{ top: topOffset }}
         >
           {children}
         </Menu.Items>
