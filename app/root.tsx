10af93b3676eb15264c166245f7d77006dbe64a5
auredagaz2023 <dev@man-super.com>
2025-12-22 23:37:58 -0800

    debug query

--- app/root.tsx
+++ app/root.tsx
@@ -423,6 +423,12 @@ const CART_QUERY = `#graphql
                 upsellingMessage: metafield(namespace: "custom", key: "upselling_message") {
                   value
                 }
+                discountPercent: metafield(namespace: "custom", key: "discount_percent") {
+                  value
+                }
+              }
+              discountPercent: metafield(namespace: "custom", key: "discount_percent") {
+                value
               }
               selectedOptions {
                 name
